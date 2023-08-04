61602f07a84eee012f92b0b780fe6f01511ba09c
Karan2310 <karandeep9619@gmail.com>
2023-08-04 18:12:46 +0530

    create root warning

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Updated import
 import "./index.css";
 import App from "./App";
 import { CookiesProvider } from "react-cookie";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")); // Use createRoot directly
 root.render(
   <React.StrictMode>
     <CookiesProvider>
